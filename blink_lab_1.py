a10d1e899ddc8014c991a68e52cb45bd5fb8b499
mjoel07 <joel.7002.m@gmail.com>
2025-01-10 08:36:46 -0800

    Create blink_lab_1.py

---
+++ blink_lab_1.py
@@ -0,0 +1,21 @@
+# Save on CPX/CPB as code.py
+# Blinks the top right RED LED
+import board
+import digitalio
+import analogio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+    
+def main():
+    print("Hello")
+            
+    while True:
+        led.value = True
+        time.sleep(0.5)
+        led.value = False
+        time.sleep(0.5)
+
+main()
